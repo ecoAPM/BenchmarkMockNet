f989d7915191eb8484b1ddafe62a77ceecc5b0df
Jason Bock <jason.r.bock@outlook.com>
2021-09-01 08:54:21 -0500

    #16 oops, gotta have all the benchmarks uncommented!

--- Program.cs
+++ Program.cs
@@ -7,19 +7,19 @@ namespace BenchmarkMockNet
     public static class Program
     {
         private static readonly BenchmarkSwitcher Runner = new(new[] {
-				//typeof(Construction),
-				//typeof(Callback),
-				//typeof(EmptyMethod),
-				//typeof(EmptyReturn),
-				//typeof(Return),
-				//typeof(Verify),
-				//typeof(CallbackOnly),
-				//typeof(EmptyMethodOnly),
-				//typeof(EmptyReturnOnly),
-				//typeof(ReturnOnly),
-				//typeof(VerifyOnly),
+				typeof(Construction),
+				typeof(Callback),
+				typeof(EmptyMethod),
+				typeof(EmptyReturn),
+				typeof(Return),
+				typeof(Verify),
+				typeof(CallbackOnly),
+				typeof(EmptyMethodOnly),
+				typeof(EmptyReturnOnly),
+				typeof(ReturnOnly),
+				typeof(VerifyOnly),
 				typeof(TwoParameters),
-				//typeof(FourParameters),
+				typeof(FourParameters),
 		  });
 
         public static void Main(string[] args)
